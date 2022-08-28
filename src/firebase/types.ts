f0054368f7f17accbd59c4e6b28c9ea2c05231d0
Viktor Solberg <viktorgsolberg@gmail.com>
2022-08-28 23:22:19 +0200

    fixed typing errors -> project should build

--- src/firebase/types.ts
+++ src/firebase/types.ts
@@ -1,24 +1,39 @@
+import firebase from 'firebase/compat';
+import UserCredential = firebase.auth.UserCredential;
+
 export interface IDocument {
     title: string;
     content: string;
 }
 
-export interface IUserInformation {
-    uId: string;
+export interface IStoredUserInformation {
+    uid: string;
     name: string;
     role: EUserRole;
     authProvider: EAuthProvider;
     email: string;
 }
 
+export type ISignedInUser = UserCredential & IStoredUserInformation;
+
 export enum EUserRole {
     USER = 'USER',
     ADMIN = 'ADMIN',
 }
 
+export const userRoleToString: Record<EUserRole, string> = {
+    USER: 'user',
+    ADMIN: 'admin',
+};
+
 export enum EAuthProvider {
     EMAIL = 'EMAIL',
     GOOGLE = 'GOOGLE',
 }
+
+export const authProviderToString: Record<EAuthProvider, string> = {
+    EMAIL: 'email',
+    GOOGLE: 'google',
+};
 
 export const emptyDocument = { content: '' } as IDocument;
